47f78a26bba258e90f049cc18ae0c00392ee3364
federico rufrancos <fde.ruf@gmail.com>
2014-11-18 22:29:09 -0300

    actualización del objectPC
    Se agregaron nos métodos necesarios para cargar los campos y evitar así
    usar tanto  el local stora

--- js/PCBuild.js
+++ js/PCBuild.js
@@ -5,35 +5,42 @@ var controllerPCBuild = (function (jsonDB) {
 		//vaya cambiando xq que el sistema tire randoms sobre los 
 		//componentes no queda muy bien
 		if(localStorage.getItem("alcanzaElDinero") == "SI"){
-			var ul = document.getElementById("PCBuildComponentes");
-			var ndd = localStorage.getItem("nivelDeDificultad");		
-			var li = document.createElement("li");
-			li.appendChild(document.createTextNode("Procesador: " + localStorage.getItem("procesador")));
-			ul.appendChild(li);
-			var li = document.createElement("li");
-			li.appendChild(document.createTextNode("Memoria ram: " + localStorage.getItem("memoriaRam")));
-			ul.appendChild(li);
-			var li = document.createElement("li");
-			li.appendChild(document.createTextNode("Disco: " + localStorage.getItem("discoRigido")));
-			ul.appendChild(li);		
-			var li = document.createElement("li");
-			li.appendChild(document.createTextNode("Fuente: " + localStorage.getItem("fuente")));
-			ul.appendChild(li);
-			var li = document.createElement("li");
-			li.appendChild(document.createTextNode("Placa madre: " + localStorage.getItem("placaMadre")));
-			ul.appendChild(li);
-			var li = document.createElement("li");
-			li.appendChild(document.createTextNode("Placa de video: " + localStorage.getItem("placaVideo")));
-			ul.appendChild(li);
-			var li = document.createElement("li");
-			li.appendChild(document.createTextNode("Dado que no pesee consumo energetico, el gabinete se deja a eleccion del usuario (ver compatibilidad con motherboard)"));
-			ul.appendChild(li);
-			var li = document.createElement("li");
-			li.appendChild(document.createTextNode("Costo total: $" + localStorage.getItem("costoTotal")));
-			ul.appendChild(li);
+				var pc = localStorage.getItem("mipc",pc);
+            console.log(pc);
+            var mipc = new PC();
+            mipc = JSON.parse(pc);
+            var ul = document.getElementById("PCBuildComponentes");
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode("Procesador: " + mipc.procesadorNombre));
+            ul.appendChild(li);
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode("Memoria ram: " + mipc.memoriaNombre));
+            ul.appendChild(li);
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode("Disco: " + mipc.discoNombre));
+            ul.appendChild(li);		
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode("Fuente: " + mipc.fuenteNombre));
+            ul.appendChild(li);
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode("Placa madre: " + mipc.placaMadreNombre));
+            ul.appendChild(li);
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode("Placa de video: " + mipc.placaVideoNombre));
+            ul.appendChild(li);
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode("Aca falto el Gabo :P gabinete, aunq no es muy util para el Green IT"));
+            ul.appendChild(li);
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode("Costo total: $" + mipc.costoTotal));
+            ul.appendChild(li);
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode("Horas de uso: " + mipc.horasDeUso));
+            ul.appendChild(li);
+		
             
-	        document.getElementById("consumoWatts").innerHTML = "El consumo de energia de la PC mostrada es de: " + localStorage.getItem("wattsTotal") + " watts";		
-			document.getElementById("gastoWatts").innerHTML = "El gasto monetario en base al consumo de energia de la PC mostrada es de: $" + localStorage.getItem("costoWattsTotal") + " por hora";
+	        document.getElementById("consumoWatts").innerHTML = "El consumo de energia de la PC mostrada es de: " + mipc.wattsTotal + " watts";		
+			document.getElementById("gastoWatts").innerHTML = "El gasto monetario en base al consumo de energia de la PC mostrada es de: $" + mipc.costoWattsTotal + " por hora";
 		}else{
 			document.getElementById("mensajeDeDineroInsuficiente").innerHTML = "En este momento el sistema no dispone de una configuracion de componentes para el uso seleccionado en el rango de precios elegido. Disculpe las molestias";
 		}
--- js/PCObject.js
+++ js/PCObject.js
@@ -34,9 +34,169 @@ function PC() {
 	placaVideoDescripcion = null,
 	placaVideoPrecio = null,
 	
-	PC.prototype.setDisco = function(nombre)
-	{
-	  this.discoNombre = nombre;
-	  alert(this.discoNombre);
+	horasDeUso = null,
+	
+	costoTotal = null,
+
+	wattsTotal = null,
+	
+	costoWattsTotal = null,
+	
+	PC.prototype.setProcesador = function(marca, jsonDB){
+	 	var i=0;
+		encuentra=false;
+		while(!encuentra && i<parseInt(jsonDB.producto.cantidadProcesadores)){
+			if(jsonDB.producto.procesadores[i].nombre == marca){
+				encuentra=true;
+				this.procesadorNombre = jsonDB.producto.procesadores[i].nombre;
+				this.procesadorConsumo = jsonDB.producto.procesadores[i].consumo;
+				this.procesadorNucleos = jsonDB.producto.procesadores[i].nucleos;
+				this.procesadorVelocidad = jsonDB.producto.procesadores[i].velocidad;
+				this.procesadorCache = jsonDB.producto.procesadores[i].cache;
+				this.procesadorPrecio = jsonDB.producto.procesadores[i].precio;
+				this.costoTotal += jsonDB.producto.procesadores[i].precio;
+				this.wattsTotal += jsonDB.producto.procesadores[i].consumo;
+			}else{
+				i++;
+			}
+			console.log(this.costoTotal);			
+		};
+	}
+	
+	PC.prototype.setMemoria = function(tagId, jsonDB){
+		var i=0;
+		encuentra=false;
+		while(!encuentra && i<parseInt(jsonDB.producto.cantidadMemoriasRam)){
+			if(jsonDB.producto.memoriasRam[i].nombre == document.getElementById(tagId).value){
+				encuentra=true;
+				this.memoriaNombre = jsonDB.producto.memoriasRam[i].nombre;
+				this.memoriaConsumo = jsonDB.producto.memoriasRam[i].consumo;
+				this.memoriaTipo = jsonDB.producto.memoriasRam[i].tipo;
+				this.memoriaCapacidad = jsonDB.producto.memoriasRam[i].capacidad;
+				this.memoriaPrecio = jsonDB.producto.memoriasRam[i].precio;
+				this.costoTotal += jsonDB.producto.memoriasRam[i].precio;
+				this.wattsTotal += jsonDB.producto.memoriasRam[i].consumo;
+			}else{
+				i++;
+			}			
+			console.log(this.costoTotal);			
+		}
+	}
+	
+	PC.prototype.setDisco = function(tagId, jsonDB){
+		var i=0;
+		encuentra=false;
+		while(!encuentra && i<parseInt(jsonDB.producto.cantidadDiscos)){
+			if(jsonDB.producto.discos[i].nombre == document.getElementById(tagId).value){
+				encuentra=true;
+				this.discoNombre = jsonDB.producto.discos[i].nombre;
+				this.discoConsumo = jsonDB.producto.discos[i].consumo;
+				this.discoCapacidad = jsonDB.producto.discos[i].capacidad;
+				this.discoTipo = jsonDB.producto.discos[i].tipo;
+				this.discoCache = jsonDB.producto.discos[i].cache;
+				this.discoPrecio = jsonDB.producto.discos[i].precio;
+				this.costoTotal += jsonDB.producto.discos[i].precio;
+				this.wattsTotal += jsonDB.producto.discos[i].consumo;
+			}else{
+				i++;
+			}			
+			console.log(this.costoTotal);			
+		}
 	};
+	PC.prototype.setFuente = function(tagId, jsonDB){
+		var i=0;
+		encuentra=false;
+		while(!encuentra && i<parseInt(jsonDB.producto.cantidadFuentes)){
+			if(jsonDB.producto.fuentes[i].nombre == document.getElementById(tagId).value){
+				encuentra=true;
+				this.fuenteNombre = jsonDB.producto.fuentes[i].nombre;
+				this.fuenteConsumo = jsonDB.producto.fuentes[i].consumo;
+				this.fuenteDescripcion = jsonDB.producto.fuentes[i].descripcion;
+				this.fuentePrecio = jsonDB.producto.fuentes[i].precio;
+				this.costoTotal += jsonDB.producto.fuentes[i].precio;
+				this.wattsTotal += jsonDB.producto.fuentes[i].consumo;
+			}else{
+				i++;
+			}			
+		}
+	};
+	PC.prototype.setPlacaMadre = function(tagId, jsonDB){
+		var i=0;
+		encuentra=false;
+		while(!encuentra && i<parseInt(jsonDB.producto.cantidadPlacasMadre)){
+			if(jsonDB.producto.placasMadre[i].nombre == document.getElementById(tagId).value){
+				encuentra=true;
+				this.placaMadreNombre = jsonDB.producto.placasMadre[i].nombre;
+				this.placaMadreConsumo = jsonDB.producto.placasMadre[i].consumo;
+				this.placaMadreSocket = jsonDB.producto.placasMadre[i].socket;
+				this.placaMadrePrecio = jsonDB.producto.placasMadre[i].precio;
+				this.costoTotal += jsonDB.producto.placasMadre[i].precio;
+				this.wattsTotal += jsonDB.producto.placasMadre[i].consumo;
+			}else{
+				i++;
+			}			
+		}
+	};
+	PC.prototype.setPlacaVideo = function(tagId, jsonDB){
+		var i=0;
+		encuentra=false;
+		while(!encuentra && i<parseInt(jsonDB.producto.cantidadPlacasVideo)){
+			if(jsonDB.producto.placasVideo[i].nombre == document.getElementById(tagId).value){
+				encuentra=true;
+				this.placaVideoNombre = jsonDB.producto.placasVideo[i].nombre;
+				this.placaVideoConsumo = jsonDB.producto.placasVideo[i].consumo;
+				this.placaVideoDescripcion = jsonDB.producto.placasVideo[i].dsecripcion;
+				this.placaVideoPrecio = jsonDB.producto.placasVideo[i].precio;
+				this.costoTotal += jsonDB.producto.placasVideo[i].precio;
+				this.wattsTotal += jsonDB.producto.placasVideo[i].consumo;
+			}else{
+				i++;
+			}			
+		}
+	};
+
+	PC.prototype.setHoras = function(horas){
+		for(var i = 0; i < horas.length; i++) {
+			if(horas[i].checked == true) {
+                this.horasDeUso=horas[i].value;
+			}
+		}
+	}
+	PC.prototype.cargarDatosIntermedio = function(jsonDB){
+		this.costoTotal = 0;
+		this.wattsTotal = 0;
+		this.setHoras(document.getElementsByName("horasDeUsoIntermedio"));
+		this.setProcesador(document.getElementById("procesadorMarca").value, jsonDB);
+		this.setMemoria("marcaRamSeleccion", jsonDB);
+		this.setDisco("discoRigidoSeleccion", jsonDB);
+		this.setFuente("fuenteSeleccion", jsonDB);
+		this.setPlacaMadre("placaMadreSeleccion", jsonDB);
+		this.setPlacaVideo("placaVideoSeleccion", jsonDB);
+		//------- TOMAMOS EL COSTO DEL KILOWATT HORA EN 0.32 CENTAVOS DE PESO
+		//------- POR LO TANTO 0.32/1000 NOS DA UN COSTO DE WATT DE 0.00032 CENTAVOS				
+		alert(this.wattsTotal);
+		alert(this.wattsTotal*0.00032);
+		this.costoWattsTotal = (this.wattsTotal*0.00032).toFixed(4);
+  }		
+	
+	PC.prototype.cargarDatosAvanzado = function(jsonDB){
+		this.costoTotal = 0;
+		this.wattsTotal = 0;
+		this.setHoras(document.getElementsByName("horasDeUsoAvanzado"));
+		var marcas = document.getElementsByName("marcasRadio");
+		for(var i = 0; i < marcas.length; i++) {
+			if(marcas[i].checked == true) {
+				marcaSeleccionada = marcas[i].value;
+			}
+		}	
+		this.setProcesador("marcaSeleccion", jsonDB);
+		this.setMemoria("marcaRamAdvSeleccion", jsonDB);
+		this.setDisco("marcaDisco", jsonDB);
+		this.setFuente("marcaFuente", jsonDB);
+		this.setPlacaMadre("marcaPlacaMadre", jsonDB);
+		this.setPlacaVideo("marcaPlacaVideo", jsonDB);
+		//------- TOMAMOS EL COSTO DEL KILOWATT HORA EN 0.32 CENTAVOS DE PESO
+		//------- POR LO TANTO 0.32/1000 NOS DA UN COSTO DE WATT DE 0.00032 CENTAVOS				
+		this.costoWattsTotal = (this.wattsTotal*0.00032).toFixed(4);
+	}
 }
